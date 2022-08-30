cb1eb68bb0b70e11975af7ca992a480e2ef53905
ekremezm <you@example.com>
2022-08-30 16:27:20 +0300

    training scripts

---
+++ scripts/train_cifar10.sh
@@ -0,0 +1,9 @@
+python3 train.py --root data \
+                 --dataset CIFAR10 \
+                 --save_path resuts \
+                 --epoch 20 \
+                 --size 32 \
+                 --batch_size 264 \
+                 --aug_mode min \
+                 --arch resnet50
+                 
---
+++ scripts/train_clothes_classification.sh
@@ -0,0 +1,8 @@
+python3 train.py --root data \
+                 --dataset SimpleFashionClassification \
+                 --epoch 50 \
+                 --size 254 \
+                 --batch_size 64 \
+                 --aug_mode big \
+                 --arch resnet101
+                 
