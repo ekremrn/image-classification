e7c4e217766d630a2e18662a8ac245a7e606411d
ekremezm <you@example.com>
2022-08-31 23:41:57 +0300

    simple graph

--- utils.py
+++ utils.py
@@ -22,3 +22,16 @@ def graph_loss_acc(epoch, loss_l, acc_l, save_img = ""):
     else:
         plt.show()
 
+
+#
+def simple_graph(x, data, x_label = "", y_label = "", save_img = ""):
+
+    plt.figure(figsize = [10, 7])
+    plt.plot(list(range(x)), data)
+    plt.xlabel(x_label, fontsize = 12)
+    plt.ylabel(y_label, fontsize = 12)
+
+    if save_img:
+        plt.savefig(save_img)
+    else:
+        plt.show()
