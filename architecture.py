eceaee9da86166008973c20d4ef1ca388ef38ef3
ekremezm <you@example.com>
2023-01-28 16:59:33 +0300

    num classes arch

--- architecture.py
+++ architecture.py
@@ -3,7 +3,5 @@ import torch.nn as nn
 
 
 def get(arch, not_pretrained, num_classes):
-    model = timm.create_model(arch, pretrained = not_pretrained)
-    model.fc = nn.Linear(2048, num_classes)
+    model = timm.create_model(arch, pretrained=not_pretrained, num_classes=num_classes)
     return model
-
